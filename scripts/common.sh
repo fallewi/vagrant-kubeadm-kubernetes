554560591ebfbffc9bfb1598e4fe5cbd889c3656
fallewi <46372386+fallewi@users.noreply.github.com>
2023-05-27 14:31:00 +0200

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -69,6 +69,7 @@ echo "CRI runtime installed susccessfully"
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
